3bc0064414dca5b8f1874422e2dd8f80c66bfae4
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-01 20:24:20 +0200

    update useSpeed hook to use toFixed for number fractions

--- src/hooks/useSpeed.js
+++ src/hooks/useSpeed.js
@@ -3,16 +3,18 @@ import {useState} from 'react' ;
 function useSpeed() {
         const [counter,setSpeed] = useState(0) ;
         function increase() {
-                setSpeed(prevCounter => {
-                        const newValue =  prevCounter + (1/100)
-                        return newValue >= 2 ? 2 : newValue ;
+                setSpeed(prevSpeed => {
+                        const newValue =  +prevSpeed + (1/100);
+                        console.log(newValue);
+                        console.log(typeof newValue) ;
+                        return newValue >= 2 ? 2 : newValue.toFixed(3) ;
                 })
         }
         function decrease() {
 
-                setSpeed(prevCounter => {
-                       const newValue =  prevCounter - (1/100)
-                        return newValue <= 0 ? 0 : newValue ;
+                setSpeed(prevSpeed => {
+                       const newValue =  +prevSpeed - (1/100)
+                        return newValue <= 0 ? 0 : newValue.toFixed(3) ;
                 });
         }
         return [counter,increase,decrease] ;
