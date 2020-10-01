9f030b70b12eaaab19e83ad5c4034335664e75e3
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-01 20:30:13 +0200

    add style to the main

--- src/components/SpeedCounter.jsx
+++ src/components/SpeedCounter.jsx
@@ -1,16 +1,22 @@
-import React from 'react' ;
-import useSpeed from "../hooks/useSpeed";
-import styles from './SpeedCounter.module.css'
-const SpeedCounter = ()=>{
-    const [speed,increase,decrease] = useSpeed();
-    return (
-        <div className={styles.div}>
-            <p>Current Speed <span>{speed}</span></p>
-            <div>
-                <button className={styles.btn} onClick={increase}>Increase Speed</button>
-                <button className={styles.btn} onClick={decrease}>Decrease Speed</button>
-            </div>
-        </div>
-    )
-}
+import React from 'react';
+import useSpeed from '../hooks/useSpeed';
+import styles from './SpeedCounter.module.css';
+const SpeedCounter = () => {
+	const [speed, increase, decrease] = useSpeed();
+	return (
+		<div className={styles.div}>
+			<p className={styles.text}>
+				Current Speed <span className={styles.current_speed}>{speed}</span>
+			</p>
+			<div>
+				<button className={styles.btn} onClick={increase}>
+					Increase Speed
+				</button>
+				<button className={styles.btn} onClick={decrease}>
+					Decrease Speed
+				</button>
+			</div>
+		</div>
+	);
+};
 export default SpeedCounter;
