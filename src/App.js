efeb5d18f1de7ba8fd6c0735b8cff61e43b845f2
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-01 19:17:07 +0200

    create links for the app with React Router Link Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';function App() {
+import React from 'react';
+import {BrowserRouter, Link} from "react-router-dom";
+
+function App() {
 	return (
 		<div>
-			<h1>This is my app</h1>
+			<BrowserRouter>
+				<AppLinks/>
+			</BrowserRouter>
 		</div>
 	);
 }
-
+const AppLinks = ()=>{
+	return (
+		<>
+			<Link to='/'>Home</Link>
+			<Link to='/About'>About</Link>
+			<Link to='/speed-counter'>Speed Counter</Link>
+		</>
+	)
+}
 export default App;
