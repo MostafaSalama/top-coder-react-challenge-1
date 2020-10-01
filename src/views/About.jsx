d310b3aab91e36e3f8caee28399df24c4eaf64cb
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-01 19:45:15 +0200

    add simple content for the about page and create some style

---
+++ src/views/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import styles from './About.module.css'
+const About = ()=>{
+    return (
+        <div className={styles.div}>
+            <h1>An App Made For You Only</h1>
+            <p>Thank You for using my app it was good to see people like you use it</p>
+        </div>
+    )
+}
+
+export default  About ;
