ba6f58dbdaa687bfe66973ab111b9bb8c2def3b6
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-01 19:43:03 +0200

    update the home page style

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react' ;
+import styles from './Home.module.css'
+import {Link} from 'react-router-dom'
 const Home = ()=>{
     return (
-        <div>
+        <div className={styles.div}>
             <h1>Welcome to my app</h1>
             <p>This is an app for hacker rank challenge</p>
+            <Link to='/speed-counter'>Speed Counter</Link>
         </div>
     )
 }
